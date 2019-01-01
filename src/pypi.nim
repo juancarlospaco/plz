3431d33d2bf0e16e3f7aaee1bf01db9fd4c12d40
juancarlospaco <juancarlospaco@gmail.com>
2019-01-01 09:34:51 -0300

    Improve the upload function with proper metadata

--- src/pypi.nim
+++ src/pypi.nim
@@ -1,5 +1,5 @@
 import
-  asyncdispatch, httpclient, strutils, xmlparser, xmltree, json, mimetypes,
+  asyncdispatch, httpclient, strutils, xmlparser, xmltree, json, mimetypes, os,
   ospaths
 
 const
@@ -79,39 +79,52 @@ proc stats*(this: PyPI | AsyncPyPI): Future[JsonNode] {.multisync.} =
     when this is AsyncPyPI: parseJson(await client.getContent(url=pypiApiUrl & "stats"))
     else: parseJson(client.getContent(url=pypiApiUrl & "stats"))
 
-# proc upload*(this: PyPI | AsyncPyPI,
-#              name, version, license, summary, description, author, maintainer: string,
-#              homepage, downloadurl, authoremail, maintaineremail, requirespython: string,
-#              filename: string, keywords: seq[string]): Future[string] {.multisync.} =
-#   ## Upload a new version of a registered package to PyPI.
-#   # Auth: https://github.com/python/cpython/blob/master/Lib/distutils/command/upload.py#L131-L135 https://warehouse.readthedocs.io/api-reference/legacy/#upload-api
-#   clientify(this)
-#
-#   var multipart_data = newMultipartData()
-#   multipart_data["protocol_version"] = "1"
-#   multipart_data[":action"] = "file_upload"
-#   multipart_data["metadata_version"] = "2.1"
-#   multipart_data["description_content_type"] = "text/markdown; charset=UTF-8; variant=GFM"
-#   multipart_data["name"] = name
-#   multipart_data["version"] = version
-#   multipart_data["license"] = license
-#   multipart_data["summary"] = summary
-#   multipart_data["description"] = description
-#   multipart_data["author"] = author
-#   multipart_data["maintainer"] = maintainer
-#   multipart_data["homepage"] = homepage
-#   multipart_data["download_url"] = downloadurl
-#   multipart_data["author_email"] = authoremail
-#   multipart_data["maintainer_email"] = maintaineremail
-#   multipart_data["requires_python"] = requirespython
-#   multipart_data["keywords"] = keywords.join(" ")
-#   multipart_data["content"] = (
-#     filename, newMimetypes().getMimetype(filename.splitFile.ext), filename.readFile)
-#   when not defined(release): echo multipart_data.repr
-#
-#   result =
-#     when this is AsyncPyPI: await client.postContent(pypiUploadUrl, multipart=multipart_data)
-#     else: client.postContent(pypiUploadUrl, multipart=multipart_data)
+proc upload*(this: PyPI | AsyncPyPI,
+             name, version, license, summary, description, author: string,
+             downloadurl, authoremail, maintainer, maintaineremail: string,
+             homepage, filename, md5_digest: string, keywords: seq[string],
+             requirespython=">=3", filetype="sdist", pyversion="source",
+             description_content_type="text/markdown; charset=UTF-8; variant=GFM",
+             ): Future[string] {.multisync.} =
+  ## Upload 1 new version of 1 registered package to PyPI from a local filename.
+  ## PyPI Upload is HTTP POST with MultipartData with HTTP Basic Auth Base64.
+  ## For some unknown reason intentionally undocumented (security by obscurity?)
+  # https://warehouse.readthedocs.io/api-reference/legacy/#upload-api
+  # github.com/python/cpython/blob/master/Lib/distutils/command/upload.py#L131-L135
+  var multipart_data = newMultipartData()
+  multipart_data["protocol_version"] = "1"
+  multipart_data[":action"] = "file_upload"
+  multipart_data["metadata_version"] = "2.1"
+  multipart_data["author"] = author
+  multipart_data["name"] = name.normalize
+  multipart_data["md5_digest"] = md5_digest # md5 hash of file in urlsafe base64
+  multipart_data["summary"] = summary.normalize
+  multipart_data["version"] = version.toLowerAscii
+  multipart_data["license"] = license.toLowerAscii
+  multipart_data["pyversion"] = pyversion.normalize
+  multipart_data["requires_python"] = requirespython
+  multipart_data["homepage"] = homepage.toLowerAscii
+  multipart_data["filetype"] = filetype.toLowerAscii
+  multipart_data["description"] = description.normalize
+  multipart_data["keywords"] = keywords.join(" ").normalize
+  multipart_data["download_url"] = downloadurl.toLowerAscii
+  multipart_data["author_email"] = authoremail.toLowerAscii
+  multipart_data["maintainer_email"] = maintaineremail.toLowerAscii
+  multipart_data["description_content_type"] = description_content_type.strip
+  multipart_data["maintainer"] = if maintainer == "": author else: maintainer
+
+  doAssert filename.existsFile, "filename must be 1 existent valid readable file"
+  let fext = filename.splitFile.ext
+  doAssert fext in ["whl", "egg", "zip"], "file extension must be 1 of .whl or .egg or .zip"
+  let mime = newMimetypes().getMimetype(fext)
+  multipart_data["content"] = (filename, mime, filename.readFile)
+  when not defined(release): echo multipart_data.repr
+
+  clientify(this)
+  # result =  # TODO: Finish this and test against the test dev pypi server.
+  #   when this is AsyncPyPI: await client.postContent(pypiUploadUrl, multipart=multipart_data)
+  #   else: client.postContent(pypiUploadUrl, multipart=multipart_data)
+  result = "result"
 
 
 when isMainModule and not defined(release):
@@ -124,20 +137,19 @@ when isMainModule and not defined(release):
   echo cliente.htmlPackage(project_name="requests")
   echo cliente.release(project_name="microraptor", project_version="2.0.0")
 
-
-  # echo cliente.upload(
-  #   name            = "TestPackage",
-  #   version         = "0.0.1",
-  #   license         = "MIT",
-  #   summary         = "A Test Package",
-  #   description     = "This should upload to PyPI",
-  #   author          = "Juan Carlos",
-  #   maintainer      = "Juan Carlos",
-  #   homepage        = "https://www.example.com",
-  #   downloadurl     = "https://www.example.com/download",
-  #   authoremail     = "author@example.com",
-  #   maintaineremail = "maintainer@example.com",
-  #   requirespython  = ">3.5",
-  #   filename        = "pyvoicechanger-1.5.0.zip",
-  #   keywords        = @["example", "keywords", "tags"],
-  # )
+  echo cliente.upload(
+    name            = "TestPackage",
+    version         = "0.0.1",
+    license         = "MIT",
+    summary         = "A test package for testing purposes",
+    description     = "A test package for testing purposes",
+    author          = "Juan Carlos",
+    downloadurl     = "https://www.example.com/download",
+    authoremail     = "author@example.com",
+    maintainer      = "Juan Carlos",
+    maintaineremail = "maintainer@example.com",
+    homepage        = "https://www.example.com",
+    filename        = "pypi.nim",
+    md5_digest      = "4266642",
+    keywords        = @["test", "testing"],
+  )
