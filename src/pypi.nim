3d032238b0e4f3eb8726d7242fa642ae6063031b
Juan Carlos <juancarlospaco@gmail.com>
2019-08-01 12:58:03 -0300

    update

--- src/pypi.nim
+++ src/pypi.nim
@@ -1,8 +1,8 @@
-hardenedBuild()
+hardenedBuild()  # Security Hardened mode.
 # For compile time code executions, we dont care the optimization or how clunky
 # it looks because is done compile time only,worse case scenario it wont compile
 const
-  pypiApiUrl* = "https://pypi.org/"                             ## PyPI Base API URL.
+  pypiApiUrl = "https://pypi.org/"                             ## PyPI Base API URL.
   pypiXmlUrl = pypiApiUrl & "pypi"                              ## PyPI XML RPC API URL.
   pypiPackagesXml = "https://pypi.org/rss/packages.xml"         ## PyPI XML API URL.
   pypiUpdatesXml = "https://pypi.org/rss/updates.xml"           ## PyPI XML API URL.
@@ -270,177 +270,142 @@ var script: string
 setControlCHook((proc {.noconv.} = quit" CTRL+C Pressed,shutting down,Bye! "))
 
 type
-  PyPIBase*[HttpType] = object ## Base object.
-    timeout*: byte  ## Timeout Seconds for API Calls, byte type, 0~255.
-    proxy*: Proxy  ## Network IPv4 / IPv6 Proxy support, Proxy type.
-  PyPI* = PyPIBase[HttpClient]           ##  Sync PyPI API Client.
-  AsyncPyPI* = PyPIBase[AsyncHttpClient] ## Async PyPI API Client.
+  PyPI = object ## Base object.
+    timeout: byte  ## Timeout Seconds for API Calls, byte type, 0~255.
+    proxy: Proxy  ## Network IPv4 / IPv6 Proxy support, Proxy type.
 
 using
   generateScript: bool
   classifiers: seq[string]
   project_name, project_version, package_name, user, release_version, destDir: string
 
-template clientify(this: PyPI | AsyncPyPI): untyped =
+template clientify(this: PyPI): untyped =
   ## Build & inject basic HTTP Client with Proxy and Timeout.
-  var client {.inject.} =
-    when this is AsyncPyPI: newAsyncHttpClient(
-      proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
-    else: newHttpClient(
-      timeout = when declared(this.timeout): this.timeout.int * 1_000 else: -1,
-      proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
+  var client {.inject.} = newHttpClient(
+    timeout = when declared(this.timeout): this.timeout.int * 1_000 else: -1,
+    proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
 
-proc newPackages*(this: PyPI | AsyncPyPI): Future[XmlNode] {.multisync.} =
+proc newPackages(this: PyPI): XmlNode =
   ## Return an RSS XML XmlNode type with the Newest Packages uploaded to PyPI.
   clientify(this)
   client.headers = headerXml
-  result =
-    when this is AsyncPyPI: parseXml(await client.getContent(pypiPackagesXml))
-    else: parseXml(client.getContent(pypiPackagesXml))
+  result = parseXml(client.getContent(pypiPackagesXml))
 
-proc lastUpdates*(this: PyPI | AsyncPyPI): Future[XmlNode] {.multisync.} =
+proc lastUpdates(this: PyPI): XmlNode =
   ## Return an RSS XML XmlNode type with the Latest Updates uploaded to PyPI.
   clientify(this)
   client.headers = headerXml
-  result =
-    when this is AsyncPyPI: parseXml(await client.getContent(pypiUpdatesXml))
-    else: parseXml(client.getContent(pypiUpdatesXml))
+  result = parseXml(client.getContent(pypiUpdatesXml))
 
-proc lastJobs*(this: PyPI | AsyncPyPI): Future[XmlNode] {.multisync.} =
+proc lastJobs(this: PyPI): XmlNode =
   ## Return an RSS XML XmlNode type with the Latest Jobs posted to PyPI.
   clientify(this)
   client.headers = headerXml
-  result =
-    when this is AsyncPyPI: parseXml(await client.getContent(pypiJobUrl))
-    else: parseXml(client.getContent(pypiJobUrl))
+  result = parseXml(client.getContent(pypiJobUrl))
 
-proc project*(this: PyPI | AsyncPyPI, project_name): Future[JsonNode] {.multisync.} =
+proc project(this: PyPI, project_name): JsonNode =
   ## Return all JSON JsonNode type data for project_name from PyPI.
   clientify(this)
   client.headers = headerJson
   let url = pypiApiUrl & "pypi/" & project_name & "/json"
-  result =
-    when this is AsyncPyPI: parseJson(await client.getContent(url=url))
-    else: parseJson(client.getContent(url=url))
+  result = parseJson(client.getContent(url=url))
 
-proc release*(this: PyPI | AsyncPyPI, project_name, project_version): Future[JsonNode] {.multisync.} =
+proc release(this: PyPI, project_name, project_version): JsonNode =
   ## Return all JSON data for project_name of an specific version from PyPI.
   clientify(this)
   client.headers = headerJson
   let url = pypiApiUrl & "pypi/" & project_name & "/" & project_version & "/json"
-  result =
-    when this is AsyncPyPI: parseJson(await client.getContent(url=url))
-    else: parseJson(client.getContent(url=url))
+  result = parseJson(client.getContent(url=url))
 
-proc htmlAllPackages*(this: PyPI | AsyncPyPI): Future[string] {.multisync.} =
+proc htmlAllPackages(this: PyPI): string =
   ## Return all projects registered on PyPI as HTML string,Legacy Endpoint,Slow.
   clientify(this)
-  result =
-    when this is AsyncPyPI: await client.getContent(url=pypiApiUrl & "simple")
-    else: client.getContent(url=pypiApiUrl & "simple")
+  result = client.getContent(url=pypiApiUrl & "simple")
 
-proc htmlPackage*(this: PyPI | AsyncPyPI, project_name): Future[string] {.multisync.} =
+proc htmlPackage(this: PyPI, project_name): string =
   ## Return a project registered on PyPI as HTML string, Legacy Endpoint, Slow.
   clientify(this)
-  result =
-    when this is AsyncPyPI: await client.getContent(url=pypiApiUrl & "simple/" & project_name)
-    else: client.getContent(url=pypiApiUrl & "simple/" & project_name)
+  result = client.getContent(url=pypiApiUrl & "simple/" & project_name)
 
-proc stats*(this: PyPI | AsyncPyPI): Future[XmlNode] {.multisync.} =
+proc stats(this: PyPI): XmlNode =
   ## Return all JSON stats data for project_name of an specific version from PyPI.
   clientify(this)
   client.headers = headerXml
-  result =
-    when this is AsyncPyPI: parseXml(await client.getContent(url=pypiStatus))
-    else: parseXml(client.getContent(url=pypiStatus))
+  result = parseXml(client.getContent(url=pypiStatus))
 
-proc listPackages*(this: PyPI | AsyncPyPI): Future[seq[string]] {.multisync.} =
+proc listPackages(this: PyPI): seq[string] =
   ## Return 1 XML XmlNode of **ALL** the Packages on PyPI. Server-side Slow.
   clientify(this)
   client.headers = headerXml
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=lppXml))
-    else: parseXml(client.postContent(pypiXmlUrl, body=lppXml))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=lppXml))
   for tagy in response.findAll("string"): result.add tagy.innerText
 
-proc changelogLastSerial*(this: PyPI | AsyncPyPI): Future[int] {.multisync.} =
+proc changelogLastSerial(this: PyPI): int =
   ## Return 1 XML XmlNode with the Last Serial number integer.
   clientify(this)
   client.headers = headerXml
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=clsXml))
-    else: parseXml(client.postContent(pypiXmlUrl, body=clsXml))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=clsXml))
   for tagy in response.findAll("int"): result = tagy.innerText.parseInt
 
-proc listPackagesWithSerial*(this: PyPI | AsyncPyPI): Future[seq[array[2, string]]] {.multisync.} =
+proc listPackagesWithSerial(this: PyPI): seq[array[2, string]] =
   ## Return 1 XML XmlNode of **ALL** the Packages on PyPI with Serial number integer. Server-side Slow.
   clientify(this)
   client.headers = headerXml
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=lpsXml))
-    else: parseXml(client.postContent(pypiXmlUrl, body=lpsXml))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=lpsXml))
   for tagy in response.findAll("member"):
     result.add [tagy.child"name".innerText, tagy.child"value".child"int".innerText]
 
-proc packageLatestRelease*(this: PyPI | AsyncPyPI, package_name): Future[string] {.multisync.} =
+proc packageLatestRelease(this: PyPI, package_name): string =
   ## Return the latest release registered for the given package_name.
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("package_releases", xmlRpcParam.format(package_name))
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=bodi))
   for tagy in response.findAll("string"): result = tagy.innerText
 
-proc packageRoles*(this: PyPI | AsyncPyPI, package_name): Future[seq[XmlNode]] {.multisync.} =
+proc packageRoles(this: PyPI, package_name): seq[XmlNode] =
   ## Retrieve a list of role, user for a given package_name. Role is Maintainer or Owner.
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("package_roles", xmlRpcParam.format(package_name))
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=bodi))
   for tagy in response.findAll("data"): result.add tagy
 
-proc userPackages*(this: PyPI | AsyncPyPI, user = user): Future[seq[XmlNode]] {.multisync.} =
+proc userPackages(this: PyPI, user = user): seq[XmlNode] =
   ## Retrieve a list of role, package_name for a given user. Role is Maintainer or Owner.
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("user_packages", xmlRpcParam.format(user))
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=bodi))
   for tagy in response.findAll("data"): result.add tagy
 
-proc releaseUrls*(this: PyPI | AsyncPyPI, package_name, release_version): Future[seq[string]] {.multisync.} =
+proc releaseUrls(this: PyPI, package_name, release_version): seq[string] =
   ## Retrieve a list of download URLs for the given release_version. Returns a list of dicts.
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("release_urls",
     xmlRpcParam.format(package_name) & xmlRpcParam.format(release_version))
-  let response =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  let response = parseXml(client.postContent(pypiXmlUrl, body=bodi))
   for tagy in response.findAll("string"):
     if tagy.innerText.normalize.startsWith("https://"): result.add tagy.innerText
 
-proc downloadPackage*(this: PyPI | AsyncPyPI, package_name, release_version,
-  destDir = getTempDir(), generateScript): Future[string] {.multisync.} =
+proc downloadPackage(this: PyPI, package_name, release_version,
+  destDir = getTempDir(), generateScript): string =
   ## Download a URL for the given release_version. Returns filename.
-  let possibleUrls = await this.releaseUrls(package_name, release_version)
+  let possibleUrls = this.releaseUrls(package_name, release_version)
   let choosenUrl = possibleUrls[0]
   assert choosenUrl.startsWith("https://"), "PyPI Download URL is not HTTPS SSL"
   let filename = destDir / choosenUrl.split("/")[^1]
   clientify(this)
   echo "⬇️\t" & choosenUrl
   if generateScript: script &= "curl -LO " & choosenUrl & "\n"
-  await client.downloadFile(choosenUrl, filename)
+  client.downloadFile(choosenUrl, filename)
   assert existsFile(filename), "file failed to download"
   echo "🗜\t", getFileSize(filename), " Bytes total (compressed)"
   if findExe"sha512sum".len > 0: echo "🔐\t" & execCmdEx(cmdChecksum & filename).output.strip
   try:
     echo "⬇️\t" & choosenUrl & ".asc"
-    await client.downloadFile(choosenUrl & ".asc", filename & ".asc")
+    client.downloadFile(choosenUrl & ".asc", filename & ".asc")
     if generateScript: script &= "curl -LO " & choosenUrl & ".asc" & "\n"
     if findExe"gpg".len > 0 and existsFile(filename & ".asc"):
       echo "🔐\t" & execCmdEx(cmdVerify & filename & ".asc").output.strip
@@ -450,32 +415,28 @@ proc downloadPackage*(this: PyPI | AsyncPyPI, package_name, release_version,
   if generateScript: script &= pipInstallCmd & filename.replace(destDir, "") & "\n"
   result = filename
 
-proc installPackage*(this: PyPI | AsyncPyPI, package_name, release_version,
-  generateScript): Future[tuple[output: TaintedString, exitCode: int]] {.multisync.} =
-  let cmd = pipInstallCmd & quoteShell(await this.downloadPackage(package_name, release_version, generateScript=generateScript))
+proc installPackage(this: PyPI, package_name, release_version,
+  generateScript): tuple[output: TaintedString, exitCode: int] =
+  let cmd = pipInstallCmd & quoteShell(this.downloadPackage(package_name, release_version, generateScript=generateScript))
   result = execCmdEx(cmd)
 
-proc releaseData*(this: PyPI | AsyncPyPI, package_name, release_version): Future[XmlNode] {.multisync.} =
+proc releaseData(this: PyPI, package_name, release_version): XmlNode =
   ## Retrieve metadata describing a specific release_version. Returns a dict.
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("release_data",
     xmlRpcParam.format(package_name) & xmlRpcParam.format(release_version))
-  result =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  result = parseXml(client.postContent(pypiXmlUrl, body=bodi))
 
-proc search*(this: PyPI | AsyncPyPI, query: Table[string, seq[string]], operator="and"): Future[XmlNode] {.multisync.} =
+proc search(this: PyPI, query: Table[string, seq[string]], operator="and"): XmlNode =
   ## Search package database using indicated search spec. Returns 100 results max.
   doAssert operator in ["or", "and"], "operator must be 1 of 'and', 'or'."
   clientify(this)
   client.headers = headerXml
   let bodi = xmlRpcBody.format("search", xmlRpcParam.format(replace($query, "@", "")) & xmlRpcParam.format(operator))
-  result =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  result = parseXml(client.postContent(pypiXmlUrl, body=bodi))
 
-proc browse*(this: PyPI | AsyncPyPI, classifiers): Future[XmlNode] {.multisync.} =
+proc browse(this: PyPI, classifiers): XmlNode =
   ## Retrieve a list of name, version of all releases classified with all of given classifiers.
   ## Classifiers must be a list of standard Trove classifier strings. Returns 100 results max.
   doAssert classifiers.len > 1, "classifiers must be at least 2 strings lenght."
@@ -486,18 +447,16 @@ proc browse*(this: PyPI | AsyncPyPI, classifiers): Future[XmlNode] {.multisync.}
     for item in classifiers: x &= xmlRpcParam.format(item)
     x
   let bodi = xmlRpcBody.format("browse", clasifiers)
-  result =
-    when this is AsyncPyPI: parseXml(await client.postContent(pypiXmlUrl, body=bodi))
-    else: parseXml(client.postContent(pypiXmlUrl, body=bodi))
+  result = parseXml(client.postContent(pypiXmlUrl, body=bodi))
 
-proc upload*(this: PyPI | AsyncPyPI,
+proc upload(this: PyPI,
              name, version, license, summary, description, author: string,
              downloadurl, authoremail, maintainer, maintaineremail: string,
              homepage, filename, md5_digest, username, password: string,
              keywords: seq[string],
              requirespython=">=3", filetype="sdist", pyversion="source",
              description_content_type="text/markdown; charset=UTF-8; variant=GFM",
-             ): Future[string] {.multisync.} =
+             ): string =
   ## Upload 1 new version of 1 registered package to PyPI from a local filename.
   ## PyPI Upload is HTTP POST with MultipartData with HTTP Basic Auth Base64.
   ## For some unknown reason intentionally undocumented (security by obscurity?)
@@ -533,11 +492,9 @@ proc upload*(this: PyPI | AsyncPyPI,
     x["maintainer"] = if maintainer == "": author else: maintainer
     x["content"] = (filename, mime, filename.readFile)
     x
-  clientify(this)
+  clientify(this) # TODO: Finish this and test against the test dev pypi server.
   client.headers = newHttpHeaders(auth)
-  result =  # TODO: Finish this and test against the test dev pypi server.
-    when this is AsyncPyPI: await client.postContent(pypiUploadUrl, multipart=multipart_data)
-    else: client.postContent(pypiUploadUrl, multipart=multipart_data)
+  result = client.postContent(pypiUploadUrl, multipart=multipart_data)
 
 proc pySkeleton() =
   ## Creates the skeleton (folders and files) for a New Python project.
@@ -588,7 +545,7 @@ proc pySkeleton() =
     writeFile(pluginName / "CHANGELOG." & ext, "# 0.0.1\n\n- First initial version of " & pluginName & "created at " & $now())
   quit("Created a new Python project skeleton, happy hacking, bye...\n", 0)
 
-proc backup*(filename: string): tuple[output: TaintedString, exitCode: int] =
+proc backup(filename: string): tuple[output: TaintedString, exitCode: int] =
   var cmd: string
   if findExe"sha512sum".len > 0:
     cmd = cmdChecksum & filename & " > " & filename & ".sha512"
@@ -646,7 +603,7 @@ proc ask2User(): auto =
     description: description,  downloadurl: downloadurl, maintainer: maintainer,
     authoremail: authoremail,  maintaineremail: maintaineremail, keywords: keywords)
 
-proc forceInstallPip*(destination: string): tuple[output: TaintedString, exitCode: int] =
+proc forceInstallPip(destination: string): tuple[output: TaintedString, exitCode: int] =
   assert destination.endswith".py", "Wrong filename for Python file"
   newHttpClient(timeout=9999).downloadFile(pipInstaller, destination) # Download
   assert existsFile(destination), "File not found: 'get-pip.py' " & destination
