8a9a696338b81cedbf927c1ec6b8a0bcd30397ba
Juan Carlos <JuanCarlospaco@gmail.com>
2021-05-17 00:30:58 -0300

    Simplify, Refactor, Fix, Revive, Improve

--- src/plz/docgen.nim
+++ src/plz/docgen.nim
@@ -1,21 +1,23 @@
 import strtabs, packages/docutils/rst, packages/docutils/rstgen, packages/docutils/rstast
 
-proc doc2html(filename: string): string {.inline.} =
+
+proc doc2html*(filename: string): string =
   assert filename.len > 0, "filename must not be empty string"
   result = rstToHtml(readFile(filename), {roSupportSmilies, roSupportRawDirective, roSupportMarkdown}, newStringTable(modeStyleInsensitive))
   assert result.len > 0, "doc2latex docgen error result must not be empty string"
   writeFile(filename & ".html", result)
 
-proc doc2latex(filename: string): string {.inline.} =
+
+proc doc2latex*(filename: string): string =
   assert filename.len > 0, "filename must not be empty string"
   result = rstToLatex(readFile(filename), {roSupportSmilies, roSupportRawDirective, roSupportMarkdown})
   assert result.len > 0, "doc2latex docgen error result must not be empty string"
   writeFile(filename & ".tex", result)
 
-proc doc2json(filename: string): string {.inline.} =
+
+proc doc2json*(filename: string): string =
   assert filename.len > 0, "filename must not be empty string"
-  option := false
-  result = renderRstToJson(rstParse(readFile(filename), "", 1, 1, option[], {roSupportSmilies, roSupportRawDirective, roSupportMarkdown}))
-  dealloc option
+  var option = false
+  result = renderRstToJson(rstParse(readFile(filename), "", 1, 1, option, {roSupportSmilies, roSupportRawDirective, roSupportMarkdown}))
   assert result.len > 0, "doc2latex docgen error result must not be empty string"
   writeFile(filename & ".json", result)
